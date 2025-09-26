a4c339225ce0d62a615a90947406394c833b0f64
Edwin van der Wal <557335@student.fontys.nl>
2025-09-26 09:49:51 +0200

    Add project files.

---
+++ ZMS.Models/Enums/TicketStatus.cs
@@ -0,0 +1,11 @@
+﻿namespace ZMS.Models.Enums
+{
+    public enum TicketStatus
+    {
+        New = 1,
+        Open =2,
+        WaitingCustomer = 3,
+        WatingSupplier = 4,
+        Solved = 5
+    }
+}
