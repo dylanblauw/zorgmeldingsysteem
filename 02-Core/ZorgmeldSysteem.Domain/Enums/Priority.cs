0fb4b1f84357d6e6b62f7f5c181505e416cbec30
Edwin van der Wal <557335@student.fontys.nl>
2025-09-26 11:20:55 +0200

    NewStructure

---
+++ 02-Core/ZorgmeldSysteem.Domain/Enums/Priority.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ZMS.Models.Enums
+{
+    public enum Priority
+    {
+        [Display(Name = "Laag")]
+        Low = 1,
+
+        [Display(Name = "Normaal")]
+        Normal = 2,
+
+        [Display(Name = "Hoog")]
+        High = 3,
+
+        [Display(Name = "Urgent")]
+        Urgent = 4,
+
+        [Display(Name = "Kritiek")]
+        Critical = 5
+    }
+}
+
---
+++ 02-Core/ZorgmeldSysteem.Domain/Enums/TicketStatus.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZorgmeldSysteem.Domain.Enums
+{
+    public enum TicketStatus
+    {
+        [Display(Name = "Open")]
+        Open = 1,
+
+        [Display(Name = "Toegewezen")]
+        Assigned = 2,
+
+        [Display(Name = "In behandeling")]
+        InProgress = 3,
+
+        [Display(Name = "Wacht op klant")]
+        PendingCustomer = 4,
+
+        [Display(Name = "Wacht op leverancier")]
+        PendingSupplier = 5,
+
+        [Display(Name = "Wacht op onderdelen")]
+        AwaitingParts = 6,
+
+        [Display(Name = "On hold")]
+        OnHold = 7,
+
+        [Display(Name = "Opgelost")]
+        Solved = 8,
+
+        [Display(Name = "Gesloten")]
+        Closed = 9,
+
+        [Display(Name = "Geannuleerd")]
+        Cancelled = 10
+    }
+}
